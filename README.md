dc1a47758cfd91e641cd94d4b32045c46f9430b4
Harsh Ganda <harshganda05@gmail.com>
2020-06-18 23:32:33 +0530

    removed redundancy and added comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TicTacToe
-Tic-Tac-Toe game build using Swing GUI in Java
+
+Tic-Tac-Toe game build using Swing GUI in Java.
+
+Build this application 2 years ago in 2018 while learning Swing GUI and thought of adding it to github.
+
+Source Code is available in src folder. To play the game you can simply open it in any Java IDE and run the application.
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,16 +1,14 @@
-import javafx.beans.binding.When;
-import javafx.scene.layout.Background;
-
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.plaf.ColorUIResource;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import static java.awt.Color.*;
+import static javax.swing.JOptionPane.*;
 
 class TicTacToe {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         UIManager.getDefaults().put("Button.disabledText",new ColorUIResource(BLUE));
         new FrameEx();
     }
@@ -18,19 +16,21 @@ class TicTacToe {
 
 class FrameEx extends JFrame implements ActionListener {
 
-    private JButton[][] box = new JButton[3][3];
-    private int[][] values = new int[3][3];
-    private JButton reset = new JButton("Reset");
+    private final JButton[][] box = new JButton[3][3];
+    private final int[][] values = new int[3][3];
     private int click=0;
-    private Label text1 = new Label("PLAYER 1");
-    private Label text2 = new Label("PLAYER 2");
+    private final Label text1 = new Label("PLAYER 1");
+    private final Label text2 = new Label("PLAYER 2");
 
-    FrameEx(){
+    FrameEx() {
 
-        for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++)
-                values[i][j]=2;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                values[i][j] = 2;
+            }
+        }
 
+        //Frame
         setTitle("Tic Tac Toe");
         setLayout(new BorderLayout(0,0));
         setBackground(WHITE);
@@ -39,189 +39,134 @@ class FrameEx extends JFrame implements ActionListener {
         JPanel player = new JPanel();
         player.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
         player.setBackground(WHITE);
-
-        text1.setBackground(RED);
-        text1.setAlignment(Label.CENTER);
-        text1.setFont(new Font("Arial", Font.BOLD,18));
-        text1.setPreferredSize(new Dimension(100,40));
-        player.add(text1);
-
-        text2.setForeground(WHITE);
-        text2.setAlignment(Label.CENTER);
-        text2.setFont(new Font("Arial", Font.BOLD,18));
-        text2.setPreferredSize(new Dimension(100,40));
-        player.add(text2);
+        labelStyle (player, text1, BLUE);
+        labelStyle (player, text2, WHITE);
 
         //Buttons
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
         panel.setBackground(WHITE);
 
-        //Reset
-        JPanel resetpanel = new JPanel();
-        resetpanel.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
-        resetpanel.setBackground(WHITE);
-
-        reset.setPreferredSize(new Dimension(100,40));
-        reset.setBackground(BLUE);
-        reset.setFont(new Font("Arial", Font.BOLD, 20));
-        reset.setForeground(WHITE);
-        resetpanel.add(reset);
-
         //Adding Panels to the Frame
         add(player, BorderLayout.NORTH);
         add(panel, BorderLayout.CENTER);
-        add(resetpanel, BorderLayout.SOUTH);
         for(int i=0;i<3;i++)
             for(int j=0; j<3; j++){
                 box[i][j]=new JButton();
-                box[i][j].setPreferredSize(new Dimension(130,120));
+                box[i][j].setPreferredSize(new Dimension(130,130));
                 box[i][j].setFont(new Font("Times New Roman", Font.PLAIN, 50));
                 box[i][j].addActionListener(this);
                 panel.add(box[i][j]);
         }
-        reset.addActionListener(this);
         setSize(500,500);
         setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    private void check_win(){
+    /* Giving styling to the Player Name panel */
+    private void labelStyle(JPanel player, Label text, Color color) {
+        text.setBackground(color);
+        text.setForeground(WHITE);
+        text.setAlignment(Label.CENTER);
+        text.setFont(new Font("Arial", Font.BOLD,18));
+        text.setPreferredSize(new Dimension(100,40));
+        player.add(text);
+    }
+
+    /* Changing the background to highlight turn of the player */
+    private void onclick(int i, int j, String s, int val, Label text1, Label text2) {
+        box[i][j].setText(s);
+        values[i][j] = val;
+        text1.setBackground(BLUE);
+        text2.setBackground(WHITE);
+    }
+
+    /* Highlighting the player's turn and checking for winner after every turn */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        click++;
+        if (e.getSource() instanceof JButton) {
+            for(int i=0;i<3;i++) {
+                for(int j=0;j<3;j++) {
+                    if (e.getSource() == box[i][j]) {
+                        if (click < 10) {
+                            box[i][j].setEnabled(false);
+                            if (click % 2 == 0) {
+                                onclick(i, j, "0", 0, text1, text2);
+                            } else {
+                                onclick(i, j, "X", 1, text2, text1);
+                            }
+                            if (click > 4)
+                                check_win();
+                        }
+                        if (click == 9) {
+                            int result = JOptionPane.showConfirmDialog(this, "Draw", "Result", JOptionPane.OK_CANCEL_OPTION);
+                            if (result == OK_OPTION) {
+                                restart();
+                            } else if (result == JOptionPane.CANCEL_OPTION) {
+                                System.exit(0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /* Conditions for winning */
+    private void check_win() {
         for(int i=0; i<3; i++) {
             if (values[i][0] == values[i][1] && values[i][1] == values[i][2] && values[i][2] == 0) {
-                int result = JOptionPane.showConfirmDialog(this,"Player 2 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-                if(result == JOptionPane.OK_OPTION) {
-                    restart();
-                }
-                else if(result == JOptionPane.CANCEL_OPTION){
-                    System.exit(0);
-                }
+                result("Player 2 Wins!");
             } else if (values[i][0] == values[i][1] && values[i][1] == values[i][2] && values[i][2] == 1) {
-                int result = JOptionPane.showConfirmDialog(this,"Player 1 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-                if(result == JOptionPane.OK_OPTION) {
-                    restart();
-                }
-                else if(result == JOptionPane.CANCEL_OPTION){
-                    System.exit(0);
-                }
+                result("Player 1 Wins!");
             }
         }
         for(int i=0; i<3; i++) {
             if (values[0][i] == values[1][i] && values[1][i] == values[2][i] && values[2][i] == 0) {
-                int result = JOptionPane.showConfirmDialog(this,"Player 2 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-                if(result == JOptionPane.OK_OPTION) {
-                    restart();
-                }
-                else if(result == JOptionPane.CANCEL_OPTION){
-                    System.exit(0);
-                }
+                result("Player 2 Wins!");
             } else if (values[0][i] == values[1][i] && values[1][i] == values[2][i] && values[2][i] == 1) {
-                int result = JOptionPane.showConfirmDialog(this,"Player 1 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-                if(result == JOptionPane.OK_OPTION) {
-                    restart();
-                }
-                else if(result == JOptionPane.CANCEL_OPTION){
-                    System.exit(0);
-                }
+                result("Player 1 Wins!");
             }
         }
-        if(values[0][0]==values[1][1] && values[1][1]==values[2][2] && values[2][2]==0){
-            int result = JOptionPane.showConfirmDialog(this,"Player 2 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-            if(result == JOptionPane.OK_OPTION) {
-                restart();
-            }
-            else if(result == JOptionPane.CANCEL_OPTION){
-                System.exit(0);
-            }
+        if(values[0][0]==values[1][1] && values[1][1]==values[2][2] && values[2][2]==0) {
+            result("Player 2 Wins!");
         }
         else
-        if(values[0][0]==values[1][1] && values[1][1]==values[2][2] && values[2][2]==1){
-            int result = JOptionPane.showConfirmDialog(this,"Player 1 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-            if(result == JOptionPane.OK_OPTION) {
-                restart();
-            }
-            else if(result == JOptionPane.CANCEL_OPTION){
-                System.exit(0);
-            }
+        if(values[0][0]==values[1][1] && values[1][1]==values[2][2] && values[2][2]==1) {
+            result("Player 1 Wins!");
         }
         else
-        if(values[2][0]==values[1][1] && values[1][1]==values[0][2] && values[0][2]==0){
-            int result = JOptionPane.showConfirmDialog(this,"Player 2 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-            if(result == JOptionPane.OK_OPTION) {
-                restart();
-            }
-            else if(result == JOptionPane.CANCEL_OPTION){
-                System.exit(0);
-            }
+        if(values[2][0]==values[1][1] && values[1][1]==values[0][2] && values[0][2]==0) {
+            result("Player 2 Wins!");
         }
         else
-        if(values[2][0]==values[1][1] && values[1][1]==values[0][2] && values[0][2]==1){
-            int result = JOptionPane.showConfirmDialog(this,"Player 1 Wins!","Winner", JOptionPane.OK_CANCEL_OPTION);
-            if(result == JOptionPane.OK_OPTION) {
-                restart();
-            }
-            else if(result == JOptionPane.CANCEL_OPTION){
-                System.exit(0);
-            }
+        if(values[2][0]==values[1][1] && values[1][1]==values[0][2] && values[0][2]==1) {
+            result("Player 1 Wins!");
         }
     }
 
-    private void restart(){
+    /* Showing result in the output window */
+    private void result(String str) {
+        UIManager.put("OptionPane.cancelButtonText", "Exit");
+        UIManager.put("OptionPane.okButtonText", "Play Again!");
+        int result = JOptionPane.showConfirmDialog(this, str,"Winner", JOptionPane.OK_CANCEL_OPTION);
+        if(result == OK_OPTION) {
+            restart();
+        }
+        else if(result == CANCEL_OPTION) {
+            System.exit(0);
+        }
+    }
+
+    /* Reset game to initial state */
+    private void restart() {
         click=0;
         for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++){
-                values[i][j]=2;
+            for(int j=0;j<3;j++) {
                 box[i][j].setEnabled(true);
-                box[i][j].setText("");
-                text1.setBackground(RED);
-                text1.setForeground(BLACK);
-                text2.setBackground(WHITE);
-                text2.setForeground(WHITE);
+                onclick(i, j,"", 2, text1, text2);
             }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e){
-        click++;
-        if (e.getSource() instanceof JButton){
-            if(e.getSource()==reset)
-                restart();
-            for(int i=0;i<3;i++)
-                for(int j=0;j<3;j++){
-                if (e.getSource()==box[i][j]){
-                    if(click<10) {
-                        box[i][j].setEnabled(false);
-                        if (click % 2 == 0) {
-                            box[i][j].setText("0");
-                            values[i][j] = 0;
-                            text1.setBackground(RED);
-                            text1.setForeground(BLACK);
-                            text2.setBackground(WHITE);
-                            text2.setForeground(WHITE);
-                        }
-                        else {
-                            box[i][j].setText("X");
-                            values[i][j] = 1;
-                            text2.setBackground(RED);
-                            text2.setForeground(BLACK);
-                            text1.setBackground(WHITE);
-                            text1.setForeground(WHITE);
-                        }
-                        if(click>4)
-                            check_win();
-                    }
-                    if(click==9){
-                        int result = JOptionPane.showConfirmDialog(this,"Draw","Result", JOptionPane.OK_CANCEL_OPTION);
-                        if(result == JOptionPane.OK_OPTION) {
-                            restart();
-                        }
-                        else if(result == JOptionPane.CANCEL_OPTION){
-                            System.exit(0);
-                        }
-                    }
-                }
-            }
-        }
     }
 }
